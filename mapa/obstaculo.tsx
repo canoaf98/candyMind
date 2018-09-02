1b79c12ef63ff2212f46cb1ecd409e9d729e562c
Andrés Felipe Cano <andresfe98@gmail.com>
2018-09-02 15:01:27 -0500

    gitignore desactivado

---
+++ mapa/obstaculo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.08.22" name="obstaculo" tilewidth="32" tileheight="32" tilecount="612" columns="12">
+ <image source="../assets/pantallaJuego/Obstaculos.png" width="392" height="1653"/>
+</tileset>
